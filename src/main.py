c3bf8d9015df5be74a2d369fa9ff869086a097bd
umikami <ggoku02@gmail.com>
2022-11-03 20:24:32 -0400

    latest

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 """
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
+from email import message
 import os
 from flask import Flask, request, jsonify, url_for
 from flask_migrate import Migrate
@@ -8,7 +9,10 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Watchlist
+import random
+import smtplib
+from email.message import EmailMessage
 #from models import Person
 
 app = Flask(__name__)
@@ -30,14 +34,98 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+@app.route('/users', methods=['GET'])
+def display_all_users():
+    users = User.query.all();
+    all_users = list(map(lambda user: user.serialize(), users))
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+    return jsonify(all_users), 200
 
-    return jsonify(response_body), 200
+@app.route('/signup', methods=['POST'])
+def create_user():
+    request_body = request.get_json(force=True)
+
+    newUser = User(email=request_body["email"], password=request_body["password"])
+    print(newUser)
+    db.session.add(newUser)
+    db.session.commit()
+
+    return jsonify("User created"), 200
+
+@app.route('/reset-password', methods=['POST'])
+def send_email():
+    global pw_code
+    request_body = request.get_json(force=True)
+
+    email = request_body["email"]
+
+    emailExists = User.query.filter_by(email=email).first()
+    admin_email = "ernestox.061@gmail.com"
+    admin_password = "feaunqxmnzhufkzl"
+
+    reset_code = random.randint(1000, 99999)
+
+    if emailExists:
+        message = EmailMessage()
+        message["Subject"] = "reset your password"
+        message["From"] = admin_email
+        message["To"] = email
+        message.set_content("reset code: " + str(reset_code))
+        with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp: 
+            smtp.login(admin_email, admin_password)
+            smtp.send_message(message)
+
+        pw_code = reset_code
+        return jsonify("email sent")
+
+@app.route('/change-password', methods=['POST'])
+def change_password():
+    request_body = request.get_json(force = True)
+    email = request_body["email"]
+    user = User.query.filter_by(email = email).first()
+
+    return jsonify(pw_code)
+
+@app.route('/watchlist-delete', methods=['DELETE'])
+def deleteStock():
+    request_body = request.get_json(force=True)
+    user_id = request_body["user_id"]
+    stock = request_body["stock"]
+
+    targetStock = Watchlist.query.filter_by(user_id=user_id, stock=stock).first()
+
+    if targetStock:
+        db.session.delete(targetStock)
+        db.session.commit()
+        return jsonify("Stock deleted succesfully."), 200
+    else:
+        return jsonify("There was an error."), 400
+
+@app.route('/watchlist-add/<new_stock>', methods=['POST'])
+def addStock(new_stock):
+    request_body = request.get_json(force=True)
+    user_id = request_body["user_id"]
+
+    user = User.query.filter_by(id=user_id).first()
+    stockExist = Watchlist.query.filter_by(user_id=user_id, stock = new_stock).first()
+
+    if user and stockExist == None:
+        newStock = Watchlist(user_id=user_id, stock=new_stock.upper(),)
+        db.session.add(newStock)
+        db.session.commit()
+        return jsonify("New stock added."), 200
+    else:    
+        return jsonify("Something went wrong. Stock might exist already."), 400
+
+@app.route('/all-watchlist', methods=['GET'])
+def getAllWatchlists():
+    watchlists = Watchlist.query.all()
+
+    readWatchlists = list(map(lambda watchlist: watchlist.serialize(), watchlists))
+
+    return jsonify(readWatchlists), 200
+
+
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -1,3 +1,4 @@
+from email.policy import default
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
@@ -6,14 +7,35 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    is_active = db.Column(db.Boolean(), unique=False, nullable=False, default=True)
+    watchlist = db.relationship('Watchlist')
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User %r>' % self.email
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
+            # do not serialize the password, its a security breach
+        }
+
+class Watchlist(db.Model):
+    __tablename__ = 'Watchlist'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey(User.id))
+    stock = db.Column(db.String(10), unique=False, nullable=False)
+
+    user = db.relationship('User', foreign_keys = [user_id])
+
+    def __repr__(self):
+        return '<Watchlist %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "stock": self.stock
             # do not serialize the password, its a security breach
         }
